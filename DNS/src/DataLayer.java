1de5b1700d8d7e55aa620469ee218df6b33b1ac6
skulkarni <ssk3315@rit.edu>
2015-12-09 14:18:48 -0500

    Initial implementation of dataLayer

--- DNS/src/DataLayer.java
+++ DNS/src/DataLayer.java
@@ -1,4 +1,10 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
@@ -56,18 +62,47 @@ public class DataLayer {
 		
 		// open file output stream 
 		// write in file here
+		FileWriter fileWriter;
+		BufferedWriter bufferedWriter;
+		try 
+		{
+			fileWriter = new FileWriter(dataFile,true);
+			bufferedWriter = new BufferedWriter(fileWriter);
+			bufferedWriter.write(str);
+			bufferedWriter.newLine();
+			bufferedWriter.flush();
+			bufferedWriter.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	
 	String lookupInCache ( String lookupEntry ) {
-		
 		// read from file here
+		FileReader fileReader;
+		try {
+			fileReader = new FileReader(dataFile);
+			BufferedReader bufferedReader =	new BufferedReader(fileReader);
+			String line;
+			while((line = bufferedReader.readLine()) != null) {
+	            String anEntry[] = line.split(" ");
+				if(anEntry[0].equals(lookupEntry))
+	            {
+	            	 return anEntry[1];
+	            }
+	        }
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		return null;
 	}
 	
-	String getNextServerToContact (){
-		
-		
-		return null;
+	String getNextServerToContact (String str){
+		int dotIndex = str.lastIndexOf('.');
+		String strToLookup = str.substring(dotIndex);
+		return lookupInCache(strToLookup);
 	}
 }
